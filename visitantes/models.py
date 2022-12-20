d99bc7578ad6be38894c112b0aa2b37b060bea9f
Samuel Felipe Costa <samuelcosta.dev@gmail.com>
2022-12-20 20:47:16 -0300

    Files
    Version 1.0

---
+++ visitantes/models.py
@@ -0,0 +1,107 @@
+from django.db import models
+
+class Visitante(models.Model):
+
+    STATUS_VISITANTE = [
+        ("AGUARDANDO", "Aguardando autorização"),
+        ("EM_VISITA", "Em visita"),
+        ("FINALIZADO", "Visita finalizada")
+    ]
+
+    status = models.CharField(
+        verbose_name="Status",
+        max_length=10,
+        choices=STATUS_VISITANTE,
+        default="AGUARDANDO"
+    )
+
+    nome_completo = models.CharField(
+        verbose_name="Nome completo",
+        max_length=194
+    )
+
+    cpf = models.CharField(
+        verbose_name="CPF",
+        max_length=11
+    )
+
+    data_nascimento = models.DateField(
+        verbose_name="Data de nascimento",
+        auto_now_add=False,
+        auto_now=False,
+    )
+
+    numero_casa = models.PositiveSmallIntegerField(
+        verbose_name="Número da casa a ser visitada",
+    )
+
+    placa_veiculo = models.CharField(
+        verbose_name="Placa de veículo",
+        max_length=7,
+        blank=True,
+        null=True,
+    )
+
+    horario_chegada = models.DateTimeField(
+        verbose_name="Horário de chegada na portaria",
+        auto_now_add=True,
+
+    )
+
+    horario_saida = models.DateTimeField(
+        verbose_name="Horário de saída do condomínio",
+        auto_now=False,
+        blank=True,
+        null=True,        
+    )
+
+    horario_autorizacao = models.DateTimeField(
+        verbose_name="Horário de autorização de entrada",
+        auto_now=False,
+        blank=True,
+        null=True,
+    )
+
+    morador_responsavel = models.CharField(
+        verbose_name="Nome do morador responsável por autorizar a entrada do visitante",
+        max_length=194,
+        blank=True,
+    )
+
+    registrado_por = models.ForeignKey(
+        "porteiros.Porteiro",
+        verbose_name="Porteiro responsável pelo registro",
+        on_delete=models.PROTECT
+    )
+
+    def get_horario_saida(self):
+        if self.horario_saida:
+            return self.horario_saida
+    
+        return "Horário de saída não registrado"
+    
+    def get_horario_autorizacao(self):
+        if self.horario_autorizacao:
+            return self.horario_autorizacao
+        
+        return "Visitante aguardando autorização"
+    
+    def get_morador(self):
+        if self.morador_responsavel:
+            return self.morador_responsavel
+        
+        return "Visitante aguardando autorização"
+    
+    def get_placa_veiculo(self):
+        if self.placa_veiculo:
+            return self.placa_veiculo
+        
+        return "Veículo não registrado"
+
+    class Meta:
+        verbose_name = "Visitante"
+        verbose_name_plural = "Visitantes"
+        db_table = "visitante"
+    
+    def __str__(self):
+        return self.nome_completo
---
+++ visitantes/views.py
@@ -0,0 +1,99 @@
+from django.contrib import messages
+from django.shortcuts import (
+    render, redirect, get_object_or_404
+)
+
+from django.http import HttpResponseNotAllowed
+
+from visitantes.models import Visitante
+from visitantes.forms import (
+    VisitanteForm, AutorizaVisitanteForm
+)
+
+from django.utils import timezone
+
+def registrar_visitante(request):
+
+    form = VisitanteForm()
+
+    if request.method == "POST":
+        form = VisitanteForm(request.POST)
+
+        if form.is_valid():
+            visitante = form.save(commit=False)
+            # commit False = não salvou no Banco de Dados ainda
+
+            visitante.registrado_por = request.user.porteiro
+            visitante.save()
+
+            messages.success(
+                request,
+                "Visitante registrado com sucesso"
+            )
+
+            return redirect("index")
+            # depois de validar e salvar o form, redireciona pro dashboard
+
+    context = {
+        "nome_pagina": "Registrar visitante",
+        "form": form,
+    }
+
+    return render(request, "registrar_visitante.html", context)
+
+def informacoes_visitante(request, id):
+    
+    visitante = get_object_or_404(Visitante, id=id)
+    
+    form = AutorizaVisitanteForm()
+    
+    if request.method == "POST":
+        form = AutorizaVisitanteForm(
+            request.POST, instance=visitante
+        )
+        
+        if form.is_valid():
+            visitante = form.save(commit=False)
+
+            visitante.status = "EM_VISITA"
+            visitante.horario_autorizacao = timezone.now()
+
+            visitante.save()
+
+            messages.success(
+                request,
+                "Entrada de visitante autorizada com sucesso"
+            )
+
+            return redirect("index")
+    
+    context = {
+        "nome_pagina": "Informações de visitante",
+        "visitante": visitante,
+        "form": form,
+    }
+    
+    return render(request, "informacoes_visitante.html", context)
+
+def finalizar_visita(request, id):
+
+    if request.method == "POST":
+        visitante = get_object_or_404(Visitante, id=id)
+
+        visitante.status = "FINALIZADO"
+        visitante.horario_saida = timezone.now()
+
+        visitante.save()
+
+        messages.success(
+            request,
+            "Visita finalizada com sucesso"
+        )
+
+        return redirect("index")
+
+    else:
+        return HttpResponseNotAllowed(
+            ["POST"],
+            "Método não permitido"
+        )
