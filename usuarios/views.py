f5950705e6a660d35ac417fc58968dd63673978a
Samuel Felipe Costa <samuelcosta.dev@gmail.com>
2022-12-20 20:46:48 -0300

    Files
    Version 1.0

---
+++ usuarios/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from visitantes.models import Visitante
+
+def index(request):
+
+    todos_visitantes = Visitante.objects.all()
+
+    context = {
+        "nome_pagina": "Início da dashboard",
+        "todos_visitantes": todos_visitantes,
+    }
+    return render(request, "index.html", context)
