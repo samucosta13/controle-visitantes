1349a1f659c25f6edddd9a3e905e6a0844a0179f
Samuel Felipe Costa <samuelcosta.dev@gmail.com>
2022-12-21 17:32:00 -0300

    New variables in context
    Variables to be called in templates

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,8 +5,23 @@ def index(request):
 
     todos_visitantes = Visitante.objects.all()
 
+    visitantes_aguardando = todos_visitantes.filter(
+        status="AGUARDANDO"
+    )
+
+    visitantes_em_visita = todos_visitantes.filter(
+        status="EM_VISITA"
+    )
+
+    visitantes_finalizado = todos_visitantes.filter(
+        status="FINALIZADO"
+    )
+
     context = {
         "nome_pagina": "Início da dashboard",
         "todos_visitantes": todos_visitantes,
+        "visitantes_aguardando": visitantes_aguardando.count(),
+        "visitantes_em_visita": visitantes_em_visita.count(),
+        "visitantes_finalizado": visitantes_finalizado.count(),
     }
     return render(request, "index.html", context)
