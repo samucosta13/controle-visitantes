fb843734023a8783fc6be6543f4fbd7cc841ba84
Samuel Felipe Costa <samuelcosta.dev@gmail.com>
2022-12-21 17:50:29 -0300

    New display rules
    Filter method

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from visitantes.models import Visitante
 
+from django.utils import timezone
+
 def index(request):
 
-    todos_visitantes = Visitante.objects.all()
+    todos_visitantes = Visitante.objects.order_by(
+        "-horario_chegada"
+        # o menos (-) indica ordem decrescente
+    )
 
     visitantes_aguardando = todos_visitantes.filter(
         status="AGUARDANDO"
@@ -17,11 +22,19 @@ def index(request):
         status="FINALIZADO"
     )
 
+    hora_atual = timezone.now()
+    mes_atual = hora_atual.month
+
+    visitantes_mes = todos_visitantes.filter(
+        horario_chegada__month=mes_atual
+    )
+
     context = {
         "nome_pagina": "Início da dashboard",
         "todos_visitantes": todos_visitantes,
         "visitantes_aguardando": visitantes_aguardando.count(),
         "visitantes_em_visita": visitantes_em_visita.count(),
         "visitantes_finalizado": visitantes_finalizado.count(),
+        "visitantes_mes": visitantes_mes.count()
     }
     return render(request, "index.html", context)
